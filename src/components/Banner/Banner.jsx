a88419a66612c50778aef04102a18cb625dc5135
anait31 <anaitullah198@gmail.com>
2024-11-05 21:58:09 +0800

    Update json data

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,16 @@
+import { Link, useNavigate } from 'react-router-dom';
 import banner from '../../assets/banner.jpg'
 const Banner = () => {
+    const navigate = useNavigate();
+    const handleNavigate = () => {
+        navigate('/dashboard')
+    }
     return (
         <div className='relative'>
             <div className='text-center bg-[#9538E2] text-white space-y-3 pb-48 pt-10 '>
                 <h1 className='text-2xl md:text-6xl font-bold w-3/4 mx-auto'>Upgrade Your Tech Accessorize with Gadget Heaven Accessories</h1>
                 <p className=''>Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
-                <button className='bg-white px-4 py-2 rounded-lg font-bold text-[#9538E2]'>Shop Now</button>
+                <button onClick={handleNavigate}  className='bg-white px-4 py-2 rounded-lg font-bold text-[#9538E2]'>Shop Now</button>
             </div>
             <div className='flex items-center justify-center mt-[-150px]'>
                 <img className='rounded-xl h-[450px] w-[1120px] object-cover' src={banner} alt="" />
