b511d43823268d8dccd3c0b880fe59450012848e
anait31 <anaitullah198@gmail.com>
2024-11-06 21:00:09 +0800

    Review code

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,11 @@
-import { Link } from "react-router-dom";
-
+import { Link, useRouteError } from "react-router-dom";
 const ErrorPage = () => {
     return (
-        <div>
-            <h2>Ooppps!</h2>
-            <Link to={'/'}>Go Back</Link>
+        <div className="grid h-[100vh] justify-center text-center items-center">
+            <div className="space-y-3">
+                <h2 className="text-3xl font-bold">Ooppps!</h2>
+                <button className="underline"><Link to={'/'}>Go Back</Link></button>
+            </div>
         </div>
     );
 };
